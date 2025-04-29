ec7ed9c484dd8e0d548c2d64342a4c5169465a4b
Yury Gribov <tetra2005@gmail.com>
2025-04-29 07:51:43 +0300

    Update effort script.

--- materials/effort.sh
+++ materials/effort.sh
@@ -15,9 +15,7 @@ for h in `grep DONE materials.md | grep -o '[0-9]\+h' | tr -d 'h'`; do
   s=$((s + h))
 done
 
-echo "Spent $(((s + 7) / 8)) workdays"
-
 done=$(grep -c 'Status:.*DONE\|Wontfix' materials.md)
 all=$(grep -c Status: materials.md)
 
-echo "$((100 * done / all))% done"
+echo "$((100 * done / all))% ($done/$all) done (spent $(((s + 7) / 8)) workdays)"
