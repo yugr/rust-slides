ef36f1f375e620e8a4f451efb2a2db6e77e60cf1
Yury Gribov <tetra2005@gmail.com>
2026-02-05 07:40:50 +0300

    Updates to PTS runner (gh-55).

--- benchmarks/cpp/PTS/benchmark.sh
+++ benchmarks/cpp/PTS/benchmark.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO:
+# - reduce stdev
+# - limit repeats for long tests (e.g. 10 min. ?)
+# - log comparator
+
 set -eu
 set -x
 
@@ -7,14 +12,22 @@ set -x
 PREFIX=$HOME/src/php-tree/root
 . $PREFIX/env
 export PHP_INI_SCAN_DIR=$PREFIX/etc/php/8.2/cli/conf.d
-PHP='php -c $PREFIX/etc/php/8.2/cli'
+PHP="php -c $PREFIX/etc/php/8.2/cli"
 
-# A list of relevant tests (popular projects, no external dependencies)
-tt='pts/apache pts/botan pts/bullet pts/c-ray pts/compress-gzip pts/compress-lz4 pts/compress-pbzip2 pts/compress-rar pts/compress-xz pts/compress-zstd pts/coremark pts/ffmpeg pts/fftw pts/gmpbench pts/luajit pts/lzbench pts/nginx pts/openjpeg pts/openssl pts/polybench-c pts/povray pts/redis pts/simdjson pts/z3 git/clickhouse'
+PTS_DIR=$HOME/src/phoronix-test-suite
+PTS_LOCAL_DIR=$HOME/.phoronix-test-suite
+
+# A list of relevant tests:
+#   - popular projects
+#   - no external dependencies (I don't have root access on server)
+#   - don't take too much time (e.g. ffmpeg, fftw, redis were removed)
+#   - produce stable results (e.g. all compress/XXX were removed)
+tt='pts/apache pts/c-ray pts/openssl pts/botan pts/bullet pts/coremark pts/gmpbench pts/nginx pts/luajit pts/polybench-c pts/povray pts/simdjson'
 
 export NO_EXTERNAL_DEPENDENCIES=TRUE
 
 for t in $tt; do
-  $PHP pts-core/phoronix-test-suite.php batch-install $t
-  $PHP pts-core/phoronix-test-suite.php batch-benchmark $t
+  $PHP $PTS_DIR/pts-core/phoronix-test-suite.php batch-install $t
+  $PHP $PTS_DIR/pts-core/phoronix-test-suite.php batch-benchmark $t
+  rm -rf $PTS_LOCAL_DIR/installed-tests/$t*
 done
