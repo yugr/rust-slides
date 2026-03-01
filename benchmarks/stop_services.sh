ee7208b0a10f9c27ff8df58ede5f5d745bff0b7f
Yury Gribov <tetra2005@gmail.com>
2026-03-01 20:37:16 +0300

    Disable motd-news too.

--- benchmarks/stop_services.sh
+++ benchmarks/stop_services.sh
@@ -5,5 +5,5 @@
 set -eu
 set -x
 
-UNITS=$(systemctl list-units --all --plain | awk '/^(apt-daily|dpkg|unattended-upgrades|update-notifier|cron|anacron|avahi|fwupd|man-db|snapd|irqbalance|cups|systemd-oomd|udisks2|logrotate)/{print $1}')
+UNITS=$(systemctl list-units --all --plain | awk '/^(apt-daily|dpkg|unattended-upgrades|update-notifier|cron|anacron|avahi|fwupd|man-db|snapd|irqbalance|cups|systemd-oomd|udisks2|logrotate|motd-news)/{print $1}')
 sudo systemctl mask $UNITS
